bd0826c7f973592ff0544b960ad7f1d6bdb90c5c
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-01-30 16:42:22 +0900

    create project

--- app/src/main/java/wear/weather/MainActivity.kt
+++ app/src/main/java/wear/weather/MainActivity.kt
@@ -2,13 +2,47 @@ package wear.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.databinding.DataBindingUtil
+import wear.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    // 서버에서 받아온 후 정렬
+    private val spinnerItems = mutableListOf("서울, 서교동", "부산", "제주")
+    private val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this,"ff",Toast.LENGTH_LONG).show()
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.activity = this
+
+        binding.mainSpinner.adapter = spinnerAdapter
+
+        binding.mainSpinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                when(position){
+                    0->{
+
+                    }
+                    1->{
+
+                    }
+                    else ->{
+
+                    }
+                }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
 
     }
 }
