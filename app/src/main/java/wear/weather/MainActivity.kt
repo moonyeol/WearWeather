5e3a8470733d45d55129cc9e97d8bffb67b41459
DESKTOP-UD2063R\thd04 <thd0427__@naver.com>
2021-01-30 15:43:22 +0900

    create project

--- app/src/main/java/wear/weather/MainActivity.kt
+++ app/src/main/java/wear/weather/MainActivity.kt
@@ -2,10 +2,13 @@ package wear.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"ff",Toast.LENGTH_LONG).show()
+
     }
 }
